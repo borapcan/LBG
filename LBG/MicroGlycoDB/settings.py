cccad56d9e6773dfc814913a316defe98e5ac9b1
brapcan <rapcanborna@gmail.com>
2025-01-24 14:12:27 +0100

    Added changes to LBG

--- LBG/MicroGlycoDB/settings.py
+++ LBG/MicroGlycoDB/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 
 
 STATIC_URL = "static/"
-MEDIA_ROOT = "/media/"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 ALLOWED_UPLOAD_TYPES = ["image/png", "image/jpeg", "image/gif", "image/svg+xml"]
